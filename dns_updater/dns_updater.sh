81a026da0c6553d4eb50af1314dabfcde2b553a5
SUB0PT1MAL <92884665+SUB0PT1MAL@users.noreply.github.com>
2024-03-15 19:59:12 +0100

    Update dns_updater.sh

--- dns_updater/dns_updater.sh
+++ dns_updater/dns_updater.sh
@@ -9,19 +9,30 @@ source canned.data
 # Check if IP changed
 if [ "$current_ip" != "$(cat fresh.data)" ]; then
     # Iterate through the list of domains
-    #IFS=',' read -ra dns_list <<< "$dns_list"
-    echo "$dns_list" | IFS=',' read -ra dns_list
-    for dns_entry in "${dns_list[@]}"; do
+    IFS=',' # Set the Internal Field Separator to comma
+    for dns_entry in $dns_list; do
         domain_name=$(echo "$dns_entry" | cut -d':' -f1)
         dns_record_id=$(echo "$dns_entry" | cut -d':' -f2)
-
         # Update the DNS record for the current domain
         curl --request PATCH \
-             --url "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$dns_record_id" \
-             --header "Content-Type: application/json" \
-             --header "Authorization: Bearer $api_token" \
-             --data "{\"content\": \"$current_ip\", \"name\": \"$domain_name\", \"proxied\": false, \"type\": \"A\", \"ttl\": 3600}"
+            --url "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$dns_record_id" \
+            --header "Content-Type: application/json" \
+            --header "Authorization: Bearer $api_token" \
+            --data "{\"content\": \"$current_ip\", \"name\": \"$domain_name\", \"proxied\": false, \"type\": \"A\", \"ttl\": 3600}"
     done
+
+    # IFS=',' read -a dns_list <<< "$dns_list"
+    # for dns_entry in "${dns_list[@]}"; do
+    #     domain_name=$(echo "$dns_entry" | cut -d':' -f1)
+    #     dns_record_id=$(echo "$dns_entry" | cut -d':' -f2)
+
+    #     # Update the DNS record for the current domain
+    #     curl --request PATCH \
+    #          --url "https://api.cloudflare.com/client/v4/zones/$zone_id/dns_records/$dns_record_id" \
+    #          --header "Content-Type: application/json" \
+    #          --header "Authorization: Bearer $api_token" \
+    #          --data "{\"content\": \"$current_ip\", \"name\": \"$domain_name\", \"proxied\": false, \"type\": \"A\", \"ttl\": 3600}"
+    # done
 
     # Execute extra command
     eval "$extra_command"
