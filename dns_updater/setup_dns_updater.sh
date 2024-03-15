9c873439a762d70878a720b3e96a95a0c92e9b76
SUB0PT1MAL <92884665+SUB0PT1MAL@users.noreply.github.com>
2024-03-15 14:47:32 +0100

    Update setup_dns_updater.sh

--- dns_updater/setup_dns_updater.sh
+++ dns_updater/setup_dns_updater.sh
@@ -16,4 +16,4 @@ echo "extra_command='$EXTRA_COMMAND'" >> canned.data
 crontab -l | { cat; echo "*/5 * * * * /usr/src/app/dns/dns_updater.sh"; } | crontab -
 
 # Start CloudFlared
-/bin/sh ../cloudflared.sh
+../cloudflared.sh
